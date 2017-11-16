a25d9b857825fd85c8879fb96b2aabc6f3b3c56a
Maxime Hauwaert <mhauwaer@ulb.ac.be>
2017-11-16 02:49:33 +0100

    New version

--- projet3.py
+++ projet3.py
@@ -1,103 +1,122 @@
 import turtle as t
 import random as r
 import time
+from copy import deepcopy
 
 Colonnes = 8
 Lignes = 20
-Epais = 30
+Epais = 35
 
-board = [[0 for _ in range(Colonnes)] for _ in range(Lignes)]
+Bricks =[None,["cyan",(None)],["blue",(None)],["orange",(None)],["yellow",((0,1),(0,0),(1,0),(1,1))],["green",(None)],["purple",(None)],["red",(None)]] 
 
-origin_x = (-Colonnes//2*Epais)
-origin_y = (-Colonnes//2*Epais-150)
+Spawn = [Colonnes//2 - 1,Lignes - 2]
+# SpawnC = (Epais/2 + Colonnes/2*Epais,Epais/2 + (Lignes-1)*Epais)
 
-spawn_x = -2*Epais
-spawn_y = origin_y + Lignes*Epais
-
-Briques = []
-
-def Board():
-	t.bgcolor("grey")
+def board():
+	t.ht()
+	t.setup(Colonnes*Epais,Lignes*Epais)
+	t.setworldcoordinates(0,0,Colonnes*Epais,Lignes*Epais)
+	t.bgcolor("black")
 	t.tracer(False)
-	column = t.Pen()
-	column.ht();column.penup();column.goto(origin_x,origin_y);column.pendown()
+
+	column = t.Pen();column.color("white")
+	column.ht();column.penup();column.goto(0,0);column.pendown()
 	column.left(90)
 	for i in range(1,Colonnes+2):
 		column.fd(Lignes*Epais)
-		column.penup();column.goto(origin_x + i*Epais,origin_y);column.pendown()
+		column.penup();column.goto(i*Epais,0);column.pendown()
 
-	row = t.Pen()
-	row.ht();row.penup();row.goto(origin_x,origin_y);row.pendown()
+	row = t.Pen();row.color("white")
+	row.ht();row.penup();row.goto(0,0);row.pendown()
 	for i in range(1,Lignes+2):
 		row.fd(Colonnes*Epais)
-		row.penup();row.goto(origin_x,origin_y + i*Epais);row.pendown()
+		row.penup();row.goto(0,i*Epais);row.pendown()
 	
 	t.update()
 
-
-def Falling():
-	y = blockp.pos()[1]
-	if block == 0:
-		y += Epais
-	return y >= origin_y + 2 * Epais
-
-def block0():
-	print('Yes')
-
-def block1():
-	print('Yes')
-
-def block2():
-	print('Yes')
-
-def block3():
-	blockp.pendown();blockp.color("yellow");blockp.begin_fill()
-	for _ in range(4):
-		blockp.fd(2*Epais);blockp.right(90)
-	blockp.end_fill()
-	blockp.penup()
-
-def block4():
-	print('Yes')
-
-def block5():
-	print('Yes')
-
-def block6():
-	print('Yes')
-
-
-def Moving_part(block):
-	# Must add an exception when a block can't be spawned --> Game lost
-	inst[block]()
-	time.sleep(1)
+def boardUpdate(Board):
+	update.clearstamps();update.shapesize(1.4,1.4)
+	for i in range(Lignes):
+		update.goto(Epais/2,Epais/2 + i * Epais)
+		for j in range(Colonnes):
+			if Board[i][j] != "black":
+				update.color(Board[i][j])
+				update.stamp()
+				update.up()
+			update.fd(Epais)
 	t.update()
-	while Falling():
-		blockp.clear();blockp.right(90);blockp.fd(Epais);blockp.left(90);time.sleep(1)
-		inst[block]()
-		t.update()
 
 
+def check(Board,brick):
+	return True
 
-	t.update()
-def Spawn(block):
-	if block in (3,4,5):
-		blockp.fd(Epais)
+def tetrisBrick():
+	#return Bricks[r.randint(1,7)]
+	return Bricks[4]
 
-	elif block == 2:
-		blockp.fd(2 * Epais)
+def movingPart(Board):
+	color,ins = tetrisBrick()
+	print(color)
+	print(ins)
+	if check(Board,None):
+		falling = True
+		i = 0
+		xy = Spawn
+		while falling:
+			BoardTemp = deepcopy(Board)
+			for coo in ins:
+				BoardTemp[xy[1]+coo[1]][xy[0]+coo[0]] = color
+			boardUpdate(BoardTemp)
+			#falling = check(Board,None)
+			time.sleep(1)
+			falling = i != 18
+			i+=1
+			xy[1] -= 1
+		Board[:] = BoardTemp
+	else:
+		print("Game Over")
+
+def checkLine(Board):
+	i = 0
+	while i < Lignes:
+		yes = True
+		j = 0
+		while yes and j < Colonnes:
+			if Board[i][j] == "black":
+				yes = False
+			else:
+				j += 1
+		if yes:
+			del Board[i]
+			Board.append(["black" for _ in range(Lignes)])
+			boardUpdate(Board)
+		else:
+			i += 1
+
+Board = [["black" for _ in range(Colonnes)] for _ in range(Lignes)]
+
+board()
+
+update = t.Pen();update.ht();update.up();update.shape("square")
 
 
-Play = True
+movingPart(Board)
 
-inst = (block0,block1,block2,block3,block4,block5,block6)
+#Board[3][2] = "yellow"
+#Board[10][4] = "blue"
+for i in range(Colonnes):
+	Board[15][i] = "red"
 
-blockp = t.Pen();blockp.ht();blockp.penup();blockp.goto(spawn_x,spawn_y)
 
-board = [[0 for _ in range(Colonnes)] for _ in range(Lignes)]
-Board()
-block = 3
-Spawn(block)
-Moving_part(block)
+for i in range(Colonnes):
+	Board[5][i] = "blue"
+
+boardUpdate(Board)
+
 input()
 
+checkLine(Board)
+
+
+
+input()
