4b428129625d07359e8f52ba149ae0743106cbba
Maxime Hauwaert <mhauwaer@ulb.ac.be>
2017-11-16 12:40:21 +0100

    check added

--- projet3.py
+++ projet3.py
@@ -7,13 +7,13 @@ Colonnes = 8
 Lignes = 20
 Epais = 35
 
+update = t.Pen();update.up();update.shape("square")
+
 Bricks =[None,["cyan",(None)],["blue",(None)],["orange",(None)],["yellow",((0,1),(0,0),(1,0),(1,1))],["green",(None)],["purple",(None)],["red",(None)]] 
 
 Spawn = [Colonnes//2 - 1,Lignes - 2]
-# SpawnC = (Epais/2 + Colonnes/2*Epais,Epais/2 + (Lignes-1)*Epais)
 
 def board():
-	t.ht()
 	t.setup(Colonnes*Epais,Lignes*Epais)
 	t.setworldcoordinates(0,0,Colonnes*Epais,Lignes*Epais)
 	t.bgcolor("black")
@@ -34,8 +34,14 @@ def board():
 	
 	t.update()
 
+def boardReset():
+	Board = [["black" for _ in range(Colonnes)] for _ in range(Lignes)]
+	boardUpdate(Board)
+	return Board
+
+
 def boardUpdate(Board):
-	update.clearstamps();update.shapesize(1.4,1.4)
+	update.ht();update.clearstamps();update.shapesize(1.4,1.4);update.up()
 	for i in range(Lignes):
 		update.goto(Epais/2,Epais/2 + i * Epais)
 		for j in range(Colonnes):
@@ -47,8 +53,15 @@ def boardUpdate(Board):
 	t.update()
 
 
-def check(Board,brick):
-	return True
+def check(Board,ins,coord):
+	yes = True
+	i = 0
+	while yes and i < 4: # Another exeptions needed
+		yes = Board[coord[1]+ins[i][1]][coord[0]+ins[i][0]] == "black"
+		if coord[1]+ins[i][1] < 0:
+			yes = False
+		i += 1
+	return yes
 
 def tetrisBrick():
 	#return Bricks[r.randint(1,7)]
@@ -56,25 +69,23 @@ def tetrisBrick():
 
 def movingPart(Board):
 	color,ins = tetrisBrick()
-	print(color)
-	print(ins)
-	if check(Board,None):
+	xy = Spawn[:]
+	if check(Board,ins,xy):
 		falling = True
-		i = 0
-		xy = Spawn
 		while falling:
 			BoardTemp = deepcopy(Board)
 			for coo in ins:
 				BoardTemp[xy[1]+coo[1]][xy[0]+coo[0]] = color
 			boardUpdate(BoardTemp)
-			#falling = check(Board,None)
-			time.sleep(1)
-			falling = i != 18
-			i+=1
+			time.sleep(0.2)
 			xy[1] -= 1
+			falling = check(Board,ins,xy)
 		Board[:] = BoardTemp
+		gameover = False
 	else:
-		print("Game Over")
+		gameover = True
+
+	return gameover
 
 def checkLine(Board):
 	i = 0
@@ -93,30 +104,37 @@ def checkLine(Board):
 		else:
 			i += 1
 
-Board = [["black" for _ in range(Colonnes)] for _ in range(Lignes)]
+def runGame():
+	board()
+	play = True
+	while play:
+		Board = boardReset()
+		gameover = False
+		while not gameover:
+			gameover = movingPart(Board)
+		print("You have lost")
+		play = int(input("Wanna play again ? (1:Yes,2:No) : "))
+		if play == 1:play = True
+		else:play = False
 
-board()
 
-update = t.Pen();update.ht();update.up();update.shape("square")
+#Board = [["black" for _ in range(Colonnes)] for _ in range(Lignes)]
+#board()
 
+runGame()
 
-movingPart(Board)
+#movingPart(Board)
 
 #Board[3][2] = "yellow"
 #Board[10][4] = "blue"
-for i in range(Colonnes):
-	Board[15][i] = "red"
+#for i in range(Colonnes):
+#	Board[15][i] = "red"
 
 
-for i in range(Colonnes):
-	Board[5][i] = "blue"
+#for i in range(Colonnes):
+#	Board[5][i] = "blue"
 
-boardUpdate(Board)
-
-input()
-
-checkLine(Board)
-
-
-
-input()
+#boardUpdate(Board)
+#input()
+#checkLine(Board)
+#input()
