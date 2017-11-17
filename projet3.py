a0cabec2c05264db53021f60e2f93212e9a2a1a9
Maxime Hauwaert <mhauwaer@ulb.ac.be>
2017-11-17 02:16:35 +0100

    Blocks can now move, WOW

--- projet3.py
+++ projet3.py
@@ -9,7 +9,23 @@ Epais = 35
 
 update = t.Pen();update.up();update.shape("square")
 
-Bricks =[None,["cyan",(None)],["blue",(None)],["orange",(None)],["yellow",((0,1),(0,0),(1,0),(1,1))],["green",(None)],["purple",(None)],["red",(None)]] 
+Bricks =[None,
+	["cyan",((-1,1),(0,1),(1,1),(2,1))],
+	["blue",((-1,1),(-1,0),(0,0),(1,0))],
+	["orange",((-1,0),(0,0),(1,0),(1,1))],
+	["yellow",((0,1),(0,0),(1,0),(1,1))],
+	["green",((-1,0),(0,0),(0,1),(1,1))],
+	["purple",((-1,0),(0,0),(0,1),(1,0))],
+	["red",((-1,1),(0,1),(0,0),(1,0))]]
+
+#Bricks =[None,
+#	["cyan",[[-1,1],[0,1],[1,1],[2,1]]],
+#	["blue",[[-1,1],[-1,0],[0,0],[1,0]]],
+#	["orange",[[-1,0],[0,0],[1,0],[1,1]]],
+#	["yellow",[[0,1],[0,0],[1,0],[1,1]]],
+#	["green",[[-1,0],[0,0],[0,1],[1,1]]],
+#	["purple",[[-1,0],[0,0],[0,1],[1,0]]],
+#	["red",[[-1,1],[0,1],[0,0],[1,0]]]]
 
 Spawn = [Colonnes//2 - 1,Lignes - 2]
 
@@ -56,32 +72,64 @@ def boardUpdate(Board):
 def check(Board,ins,coord):
 	yes = True
 	i = 0
-	while yes and i < 4: # Another exeptions needed
-		yes = Board[coord[1]+ins[i][1]][coord[0]+ins[i][0]] == "black"
-		if coord[1]+ins[i][1] < 0:
+	while yes and i < 4:
+		if coord[0]+ins[i][0] >= 0 and coord[0]+ins[i][0] < Colonnes:
+			yes = Board[coord[1]+ins[i][1]][coord[0]+ins[i][0]] == "black"
+			if coord[1]+ins[i][1] < 0:
+				yes = False
+			i += 1
+		else:
 			yes = False
-		i += 1
 	return yes
 
 def tetrisBrick():
-	#return Bricks[r.randint(1,7)]
-	return Bricks[4]
+	return Bricks[r.randint(1,7)]
 
 def movingPart(Board):
+
+	def right():
+		coord = (xy[0] + 1,xy[1])
+		if check(Board,ins,coord):
+			xy[0] += 1
+	
+	def left():
+		coord = (xy[0] - 1,xy[1])
+		if check(Board,ins,coord):
+			xy[0] -= 1
+
+	def down():
+		It[:] = [1]
+
+	def up():
+		pass # TODO
+
 	color,ins = tetrisBrick()
 	xy = Spawn[:]
+	It = [100]
 	if check(Board,ins,xy):
 		falling = True
+		t.onkey(right,"Right")
+		t.onkey(left,"Left")
+		t.onkey(down,"Down")
+		t.onkey(up,"Up")
+		t.listen()
+		i = 0
 		while falling:
 			BoardTemp = deepcopy(Board)
 			for coo in ins:
 				BoardTemp[xy[1]+coo[1]][xy[0]+coo[0]] = color
 			boardUpdate(BoardTemp)
-			time.sleep(0.2)
-			xy[1] -= 1
+
+			if i >= It[0]:
+				xy[1] -= 1
+				i = 0
+				It = [100]
+
 			falling = check(Board,ins,xy)
+			i += 1
 		Board[:] = BoardTemp
 		gameover = False
+		time.sleep(0.75)
 	else:
 		gameover = True
 
@@ -112,29 +160,11 @@ def runGame():
 		gameover = False
 		while not gameover:
 			gameover = movingPart(Board)
+			checkLine(Board)
 		print("You have lost")
 		play = int(input("Wanna play again ? (1:Yes,2:No) : "))
 		if play == 1:play = True
 		else:play = False
 
-
-#Board = [["black" for _ in range(Colonnes)] for _ in range(Lignes)]
-#board()
-
-runGame()
-
-#movingPart(Board)
-
-#Board[3][2] = "yellow"
-#Board[10][4] = "blue"
-#for i in range(Colonnes):
-#	Board[15][i] = "red"
-
-
-#for i in range(Colonnes):
-#	Board[5][i] = "blue"
-
-#boardUpdate(Board)
-#input()
-#checkLine(Board)
-#input()
+if __name__ == "__main__":
+	runGame()
